f211c6da94656d8cc98274126ea835087f0c9e58
JairoCruz <azaveliz@gmail.com>
2017-03-21 14:55:45 -0600

    Add React Boostrap, delete default Files, and create index.js file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<div>Goal App</div>, document.getElementById('root'))
