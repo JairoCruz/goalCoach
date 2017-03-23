d60f0af6dc2dfe25297d53604d59e40d08a13a1a
JairoCruz <azaveliz@gmail.com>
2017-03-23 12:19:19 -0600

    63, Adding Redux To GoalCoach, instead work us in add history.push and history.replace, for change the view or component. I change Router/Route/BrowserRouter, for Router/Route/React-Router. It works!!!

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { Router, Route } from 'react-router';
+import createBrowserHistory from 'history/createBrowserHistory';
+
 import { firebaseApp } from './firebase';
 
 import App from './components/App';
@@ -8,18 +10,23 @@ import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 
 
+const history = createBrowserHistory();
+
 firebaseApp.auth().onAuthStateChanged(user => {
 	if (user) {
 		console.log('user has signed in or up', user);
+		history.push('/app');
 	} else {
 		console.log('user has signed out or still needs to sign in.');
+		history.replace('/signin');
 	}
 })
 
+
+
 ReactDOM.render(
-	<Router>
+	<Router path={'/'} history={history}>
 	<div>
-		<Route exact path="/" component={App} />
 		<Route path="/app" component={App} />
 		<Route path="/signin" component={SignIn} />
 		<Route path="/signup" component={SignUp} />
